a02df3bab00699d82153bf283567ec629f50f7dd
JanaHazem12 <jana.hazem.abbass@gmail.com>
2025-03-26 01:59:46 +0200

    Submit LAB_1_Task_1

---
+++ LAB_1_Task_1/books.js
@@ -0,0 +1,74 @@
+const books = [
+    {
+      ISBN: "978-0143128540",
+      name: "Hide And Seek",
+      category: "History",
+      image: "https://marketplace.canva.com/EAFf0E5urqk/1/0/1003w/canva-blue-and-green-surreal-fiction-book-cover-53S3IzrNxvY.jpg",
+      pages: 20,
+      author: "Yuval Noah Harari",
+      price: 19.99
+    },
+    {
+      ISBN: "978-0062316097",
+      name: "Alone A True Story",
+      category: "Fiction",
+      image: "https://marketplace.canva.com/EAFPHUaBrFc/1/0/1003w/canva-black-and-white-modern-alone-story-book-cover-QHBKwQnsgzs.jpg",
+      pages: 208,
+      author: "Paulo Coelho",
+      price: 14.99
+    },
+    {
+      ISBN: "978-0307743657",
+      name: "a million to one",
+      category: "Dystopian",
+      image: "https://www.designforwriters.com/wp-content/uploads/2017/10/design-for-writers-book-cover-tf-2-a-million-to-one.jpg",
+      pages: 328,
+      author: "George Orwell",
+      price: 12.99
+    },
+    {
+      ISBN: "978-0062457714",
+      name: "beyond the ocean door",
+      category: "Self-Help",
+      image: "https://blog-cdn.reedsy.com/directories/gallery/248/large_65b0ae90317f7596d6f95bfdd6131398.jpg",
+      pages: 30,
+      author: "James Clear",
+      price: 16.99
+    },
+    {
+      ISBN: "978-0307474278",
+      name: "the design of books",
+      category: "Psychology",
+      image: "https://tmm.chicagodistributioncenter.com/IsbnImages/9780226822952.jpg",
+      pages: 400,
+      author: "Charles Duhigg",
+      price: 18.50
+    },
+    {
+      ISBN: "978-0590353427",
+      name: "sin eater",
+      category: "Fantasy",
+      image: "https://s26162.pcdn.co/wp-content/uploads/2020/01/Sin-Eater-by-Megan-Campisi.jpg",
+      pages: 320,
+      author: "J.K. Rowling",
+      price: 10.99
+    },
+    {
+      ISBN: "978-0679783268",
+      name: "abstract minds",
+      category: "Classic",
+      image: "https://thebookcoverdesigner.com/wp-content/uploads/2019/12/Abstract-Minds-ebook.jpg",
+      pages: 30,
+      author: "Jane Austen",
+      price: 9.99
+    },
+    {
+      ISBN: "978-1451673319",
+      name: "reform a mermaid journey",
+      category: "Science Fiction",
+      image: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/an-abstract-creative-book-cover-design-template-e270f65594b616c7e4de0dd4d68c92f7_screen.jpg?ts=1637001582",
+      pages: 256,
+      author: "Ray Bradbury",
+      price: 11.99
+    }
+  ];
